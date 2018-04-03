4d290fa3d0156d47aae847c10d703013c3088957
Franky Ho <franky@sentronic.hk>
2018-04-03 02:23:11 +0000

    second repository

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -11,4 +11,8 @@ class StaticpagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get about" do
+    get staticpages_about_url
+    assert_response :success
+  end
 end
