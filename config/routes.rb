e7ea5753671bf545412e6b3dcb988a43922d5327
Franky Ho <franky@sentronic.hk>
2018-04-10 09:51:08 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
+#   get 'users/new' #still need or delete? 20180410
 
-  get 'example/user'
+#  get 'example/user'  #still need or delete? 20180410
 
   root 'staticpages#home'
   
   get  '/help', to: 'staticpages#help'
   get  '/about', to: 'staticpages#about'
   get  '/contact', to: 'staticpages#contact'
-   get  '/signup',  to: 'users#new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
