96568ac43dcabfd8c7efa307be58d3eee16ce983
Franky Ho <franky@sentronic.hk>
2018-04-10 02:55:54 +0000

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+    before_save { email.downcase! }
+  #  self.email = email.downcase #6.32
+   validates :name, presence: true #6.9：为 name 属性添加存在性验证 GREEN
+   validates :email, presence: true
+   
+     validates :name,  presence: true, length: { maximum: 50 } #6.16：为 name 属性添加长度验证 GREEN
+     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+     validates :email, presence: true, length: { maximum: 255 },
+                       format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: { case_sensitive: false }
+
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end
