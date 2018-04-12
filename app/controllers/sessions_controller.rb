2c29cbe8cd74b0d0efb12c6b26f04ae9d5018ff3
Franky Ho <franky@sentronic.hk>
2018-04-12 04:21:18 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,13 @@ class SessionsController < ApplicationController
   end
  
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    
+      
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
       
       # 登入用户，然后重定向到用户的资料页面
     else
@@ -16,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
   
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,20 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+  # 登入指定的用户
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end  
+  class ActionDispatch::IntegrationTest
+
+  # 登入指定的用户
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+  
   
   include ApplicationHelper  ## 代码清单 5.35：把 Application 辅助模块引入测试
 
