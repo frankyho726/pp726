967461863fa34c73d1d2023839b11df4b5dd459d
Franky Ho <franky@sentronic.hk>
2018-04-28 10:54:36 +0000

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3 的配置
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV[AKIAJD3RIUBJYS2WSGWA],
-      :aws_secret_access_key => ENV[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]
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV[pickpark]
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
